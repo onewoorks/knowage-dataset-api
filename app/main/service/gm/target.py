6a84670d938c14c0a2121afd6d694c401810f017
onewoorks <irwanbibrahim@onewoorks.com>
2019-09-10 16:45:35 +0800

    remove decimal

--- app/main/service/gm/target.py
+++ app/main/service/gm/target.py
@@ -70,7 +70,7 @@ class GM_Target():
             content[100] = kl
             for m in month:
                 index = 101 + m
-                content[index] = str(data[kl][common_method.GetMonthName(m+1)])
+                content[index] = "{0:,.0f}".format(int(data[kl][common_method.GetMonthName(m+1)]))
             data_list.append(content)
         return data_list
 
