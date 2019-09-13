c50bd37cf030e355dbcb13876eb3249c30cf76ed
onewoorks <irwanbibrahim@onewoorks.com>
2019-09-13 12:59:25 +0800

    gm revenue prediction

--- app/main/service/gm/revenue.py
+++ app/main/service/gm/revenue.py
@@ -1,4 +1,5 @@
 from ...model.mysql.government_management import MYSQL_GM_QUERY
+from ..predictions.linear_regression import Lr_Predict
 
 import json
 
@@ -11,4 +12,19 @@ class GM_Revenue():
         
     def Revenue_Pivot(self):
         data = mysql_query.Get_Latest_WS('GM_REVENUE_TARGET_ACTUAL')
-        return json.loads(data[0]['ws_data'])
+        pivot_data = json.loads(data[0]['ws_data'])
+        x_value = []
+        y_value = []
+        scale = 100000
+        for d in pivot_data:
+            if d['actual_cumulative'] != "":
+                x_value.append(d['index'])
+                y_value.append(float(d['actual_cumulative'])/scale)
+
+        pred_lr = Lr_Predict()
+        predict = pred_lr.simple_prediction_pivot(x_value, y_value, 12)
+        
+        for p in pivot_data:
+            pivot_data[p['index']-1]['predict'] = str(predict[p['index']-1]*scale)
+        
+        return pivot_data
--- app/main/service/predictions/linear_regression.py
+++ app/main/service/predictions/linear_regression.py
@@ -17,3 +17,18 @@ class Lr_Predict:
             prediction[no] = pred[0]
             no += 1
         return prediction
+
+    def simple_prediction_pivot(self, x_value, y_value, x_max):
+        x = np.array(x_value).reshape((-1,1))
+        y = np.array(y_value)
+
+        model = LinearRegression().fit(x,y)
+
+        x_new = np.arange(x_max).reshape((-1,1))
+        y_pred = model.predict(x_new).reshape((-1,1))
+
+        prediction = []
+        for i in y_pred:
+            prediction.append(i[0])
+
+        return prediction
