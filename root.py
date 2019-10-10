ca6aa43ea2f86724fdb1f8142374d8daba158acf
onewoorks <irwanbibrahim@onewoorks.com>
2019-10-10 10:19:18 +0800

    created upload folder if not exist

--- root.py
+++ root.py
@@ -2,12 +2,19 @@ from flask import Flask
 from werkzeug.middleware.proxy_fix import ProxyFix
 from app.main.controller import api
 from flask_cors import CORS, cross_origin
+import os
+from pathlib import Path
+
+config_file = Path('app/config.cfg')
 
 app = Flask(__name__)
 
 CORS(app)
 app.wsgi_app = ProxyFix(app.wsgi_app)
-app.config.from_pyfile('app\config.cfg')
+app.config.from_pyfile(config_file)
+
+if not Path(app.config['UPLOAD_MEDIA']).exists():
+    os.mkdir(app.config['UPLOAD_MEDIA'])
 
 api.init_app(app)
 app.run(debug=True,host="0.0.0.0") 
