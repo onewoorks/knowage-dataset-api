55a48a7e055f90bc395ff9d1e64eb1237fd150d9
onewoorks <irwanbibrahim@onewoorks.com>
2019-10-15 17:17:09 +0800

    refix

--- README.md
+++
@@ -1,5 +0,0 @@
-install modules
-`pip install -r requirements.txt`
-
-run application
-`python root.py`
--- app/main/model/common.py
+++
@@ -1,5 +0,0 @@
-from . import execute_query, insert_ws_data
-
-class Common_Query:
-    def register_ws(self,input):
-        insert_ws_data(input)
