f86edd4e8f723efe5a0e1c6b2215e56170e99d83
onewoorks <irwanbibrahim@onewoorks.com>
2019-10-17 17:55:23 +0800

    update training statistic

--- app/main/service/training/epol.py
+++ app/main/service/training/epol.py
@@ -53,6 +53,52 @@ class EpolServices(EpolSetter):
     def get_training_summary_report_cbt(self):
         return self.__training_summary_report_cbt()
 
+    def __training_dateset_pivot(self, payloads):
+        clean = pd.DataFrame(payloads)
+        pivot = clean[clean['101'] == 'Total'].reset_index().drop('index',axis=1)
+        pivot_data = []
+        for p in range(len(pivot)):
+            content = {}
+            content_summary = []
+            for r in range(12):
+                content_summary.append({
+                    "id" : r+1,
+                    "month" : r+1,
+                    "value" : pivot.loc[p][str(102+r)]
+                })
+            content_cummulative = []
+            cumsum = pivot.loc[p].drop(labels=['100','101','114']).cumsum()
+            for c in range(len(cumsum)):
+                content_cummulative.append({
+                    "id" : c+1,
+                    "month" : c+1,
+                    "value" : cumsum[str(102+c)]
+                })
+            content[pivot.loc[p]['100']] = {
+                    "monthly" : content_summary,
+                    "total" : pivot.loc[p]['114'],
+                    "cummulative" : content_cummulative
+            }
+            pivot_data.append(content)
+        return pivot_data
+
+    def __training_dataset_total(self, payloads):
+        type_user_summary = pd.DataFrame(payloads).drop('101',axis=1)
+        for c in range(len(type_user_summary.columns.values) - 1):
+            type_user_summary[str(102+c)] = type_user_summary[str(102+c)].str.replace(",", "").astype(float)
+        summary = type_user_summary.groupby('100').sum()
+        total = []
+        for s in range(len(summary)):
+            content = {}
+            content['100'] = summary.iloc[s].name
+            content['101'] = 'Total'
+            start_no = 102
+            for t in summary.iloc[s].to_dict():
+                content[str(start_no)] = "{:,.0f}".format(summary.iloc[s][t])
+                start_no += 1
+            total.append(content)
+        return total
+
     def __training_construct_dataset(self, payloads):
         aa = payloads.sum()
         ab = aa.loc[:,['total']].to_dict()
@@ -67,10 +113,11 @@ class EpolServices(EpolSetter):
                 grand_total = 0
                 for month in range(12):
                     grand_total += month_count['total'][(month+1)] if month+1 in month_count['total'] else 0
-                    content[str(start_no)] = month_count['total'][(month+1)] if month+1 in month_count['total'] else 0
+                    content[str(start_no)] = "{0:,.0f}".format(month_count['total'][(month+1)]) if month+1 in month_count['total'] else "0"
                     start_no += 1
-                content['114'] = grand_total
+                content['114'] = "{0:,.0f}".format(grand_total)
                 response.append(content)
+        response = response + self.__training_dataset_total(response) 
         return response
 
     def __training_summary_report_cbt(self):
@@ -94,14 +141,16 @@ class EpolServices(EpolSetter):
         ws_name     = "EPOL STATISTIC"
         print('-- {} --'.format(ws_name))
         print('-- start query----')
+        cbt = self.__training_summary_report_cbt()
+        latihan_dalam_kelas = self.__training_summary_report_latihan()
         dataset = {
             "datatable" : {
-                "cbt"   : self.__training_summary_report_cbt(),
-                "latihan_dalam_kelas" : self.__training_summary_report_latihan()
+                "cbt"   : cbt,
+                "latihan_dalam_kelas" : latihan_dalam_kelas
             },
             "cockpit"   : {
-                "cbt"                   : {},
-                "latihan_dalam_kelas"   : {}
+                "cbt"                   : self.__training_dateset_pivot(cbt),
+                "latihan_dalam_kelas"   : self.__training_dateset_pivot(latihan_dalam_kelas)
             }
         }
         end_time    = datetime.now()
